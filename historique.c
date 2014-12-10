aa01b4fc93df948ed73fc4fb94cd287aa553363f
sanganne <alexandre.martin@viacesi.fr>
2014-12-10 11:54:22 +0100

    menu historique

---
+++ historique.c
@@ -0,0 +1,35 @@
+#include <stdio.h> //on va dans les bibliothèques stdio et stdlib.
+#include <stdlib.h>
+#define TAILLE_MAX 1000 //Taille du tableau (ici il est de taille 1000)
+
+int main(int argc, char *argv[])
+{
+    FILE* historique = NULL; //on initialise le pointeur à NULL.
+
+    char chaine[TAILLE_MAX] = ""; //chaine vide de TAILLE_MAX.
+
+
+    historique = fopen("historique.txt", "r+");  //non du fichier pointé.
+
+    if (historique != NULL)
+    {
+        while (fgets(chaine, TAILLE_MAX, historique) != NULL) //On lit le fichier tant que l'on ne reçoit pas (NULL).
+        {
+            printf("%s", chaine); //on affiche la chaîne que l'on vient de lire.
+            fseek(historique,0, SEEK_SET);
+
+        }
+
+
+        fclose(historique); //On ferme le fichier ouvert.
+    }
+    else
+    {
+
+        printf("Il n'y a aucune donnee sauvegardee"); //message d'erreur si il n'y a pas encore de sauvegarde.
+
+    }
+
+    return 0;
+}
+
--- menu.c
+++ menu.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int menu(choix)
+#include <string.h>
+#define TAILLE_MAX 1000
+int menu(int choix, FILE* historique)
 {
     switch (choix)
     {
         case 1:
 
         {
-            printf("choix de la difficulté\n");
+            printf("choix de la difficulte\n");
             int choix = 0;
             printf("1.Facile pour débutant\n");
             printf("2.Intermediaire pour joueurs habitues\n");
             printf("3.Difficile pour un vrai defis\n");
             scanf ("%d", &choix);
 
+            fprintf (historique, "choix de la difficulte:%d\n", choix);
+
             if (choix ==1)
             {
                 printf("1.1ere grille\n");
-                printf("2;2eme grille\n");
+                printf("2.2eme algrille\n");
                 printf("3.3eme grille\n");
                 scanf ("%d", &choix);
             }
             if (choix ==2)
             {
                 printf("1.1ere grille\n");
-                printf("2;2eme grille\n");
+                printf("2.2eme grille\n");
                 printf("3.3eme grille\n");
                 scanf ("%d", &choix);
             }
             if (choix ==3)
             {
                 printf("1.1ere grille\n");
-                printf("2;2eme grille\n");
+                printf("2.2eme grille\n");
                 printf("3.3eme grille\n");
                 scanf ("%d", &choix);
             }
+
+                fprintf (historique, "numero de grille%d\n", choix);
+
 
 
 
@@ -66,27 +72,52 @@ int menu(choix)
     }
 }
 
-int main ()
+int main (int argc, char *argv [])
 {
 
+    FILE* historique= NULL;
+    historique = fopen("historique.txt", "r+");
 
     char pseudo [50];
+    int difficulte [3]= {0};
+    int grille [3]= {0};
     printf("____________________\n");
     printf("Bienvenue sur Hanjie\n");
     printf("--------------------\n");
     printf("quel est votre pseudo ?\n");
     scanf("%s", &pseudo );
+
+    fprintf (historique, "pseudo:%s\n", pseudo);
+
+
     int choix = 0;
     printf("1. Niveau de difficulte\n");
     printf("2. Jouer au Hanjie\n");
     printf("3. Historique de vos parties\n");
-    printf ("4.Quitter\n");
+    printf ("4.Manuel\n");
+    printf ("5.quitter\n");
     scanf("%d", &choix);
+
     if (choix == 4)
+    {
+        printf("Bienvenue dans le manuel d'Hanjie. Nous allons vous expliquer comment jouer:\n");
+        printf("Le jeu est composé de trois niveaux de difficultés.\n");
+        printf("Pour chaque niveau, vous avez trois grilles disponibles.\n");
+        printf("Le but est simple, trouver le dessin caché dans chaque grille.\n");
+        printf("Pour cela, vous devrez vous servir des chiffes indiqués en haut et à gauche de la grille.\n");
+        printf("Ces chiffres vous indiquent le nombres de cases à griser par lignes et colonnes.\n");
+        printf("Pour griser les cases, vous devez indiquer leurs positions en mettant un espace entre chaques cases.\n");
+        printf("Par exemple: A1 B6 D4.\n");
+    }
+
+    if (choix == 5)
     {
         printf("Au revoir et à bientot!");
         return 0;
     }
-    menu(choix);
+    menu(choix,historique);
 
+
+
+   fclose(historique);
 }
